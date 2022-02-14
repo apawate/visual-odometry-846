c89351ccc1fcfa8d02e4875134d55743505bbe48
Agastya Pawate <73004127+apawate@users.noreply.github.com>
2022-02-13 16:26:12 -0800

    Updated with the working ZED code.

--- positional_tracking.py
+++ positional_tracking.py
@@ -1,11 +1,10 @@
-
 """
-    This sample shows how to track the position of the ZED camera 
+    This sample shows how to track the position of the ZED camera
     and displays it in a OpenGL window.
 """
 
 import sys
-import pillow
+
 from PIL import Image
 import ogl_viewer.tracking_viewer as gl
 import pyzed.sl as sl
@@ -16,14 +15,14 @@ import time
 
 
 def Tracking(p0, p1):
-    image = PIL.Image.open("field.png")
+    image = cv2.imread('field.png')
     blank_image = np.array(image)
-    p1*=370
-    p0*=800
-    p0=round(p0)
-    p1=round(p1)
+    p1 *= 75.07091243
+    p0 *= 75.07091243
+    p0 = round(p0)
+    p1 = round(p1)
 
-    cv2.circle(blank_image, (p0+600, p1+600),4, (0, 0, 0), 2)
+    cv2.circle(blank_image, (p0 + 900, p1 + 335), 4, (0, 0, 0), 2)
     plt.imshow(blank_image)
     cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
 
@@ -32,17 +31,12 @@ def Tracking(p0, p1):
     cv2.waitKey(1)
 
 
-
-
-
-
-
 if __name__ == "__main__":
 
     init_params = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,
-                                 coordinate_units=sl.UNIT.METER,
-                                 coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
-                                 
+                                    coordinate_units=sl.UNIT.METER,
+                                    coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
+
     # If applicable, use the SVO given as parameter
     # Otherwise use ZED live stream
     if len(sys.argv) == 2:
@@ -88,29 +82,23 @@ if __name__ == "__main__":
         if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
             tracking_state = zed.get_position(camera_pose)
             if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:
-
-
                 rotation = camera_pose.get_rotation_vector()
 
                 translation = camera_pose.get_translation(py_translation)
                 text_rotation = str((round(rotation[0], 2), round(rotation[1], 2), round(rotation[2], 2)))
-                text_translation = str((round(translation.get()[0], 2), round(translation.get()[1], 2), round(translation.get()[2], 2)))
+                text_translation = str(
+                    (round(translation.get()[0], 2), round(translation.get()[1], 2), round(translation.get()[2], 2)))
                 pose_data = camera_pose.pose_data(sl.Transform())
 
-                Tracking(round(translation.get()[0], 2),round(translation.get()[2], 2))
+                Tracking(round(translation.get()[0], 2), round(translation.get()[2], 2))
                 viewer.updateData(pose_data, text_translation, text_rotation, tracking_state)
 
                 numFrames = numFrames + 1
-                #print(round((((round(translation.get()[0], 2)**2)+(round(translation.get()[2], 2)**2))**0.5)*39.66010468,2))
-                #(round((((round(translation.get()[0], 2) ** 2) + (round(translation.get()[2], 2) ** 2)) ** 0.5) * 39.66010468, 2))
-                #print(round((((abs((round(translation.get()[0], 2)))**2)+(abs(4+(round(translation.get()[2], 2)))**2))**.5)*39.66010468,2))
-                print(round((((abs((round(translation.get()[0], 2)))**2)+(abs((round(translation.get()[2], 2)))**2))**.5)*39.66010468,2))
-
-
+                # print(round((((round(translation.get()[0], 2)**2)+(round(translation.get()[2], 2)**2))**0.5)*39.66010468,2))
+                # (round((((round(translation.get()[0], 2) ** 2) + (round(translation.get()[2], 2) ** 2)) ** 0.5) * 39.66010468, 2))
+                # print(round((((abs((round(translation.get()[0], 2)))**2)+(abs(4+(round(translation.get()[2], 2)))**2))**.5)*39.66010468,2))
+                print(round((((abs((round(translation.get()[0], 2))) ** 2) + (
+                            abs((round(translation.get()[2], 2))) ** 2)) ** .5) * 39.66010468, 2))
 
     viewer.exit()
     zed.close()
-
-
-
-
